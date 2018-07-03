94ef2763ac0ea152154b5012134e8d3ab4c4bff2
elena-gancheva <eagan4eva@gmail.com>
2018-07-03 21:53:17 +0300

    [client] init react client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+    }
+
+    render() {
+        return (
+            <div style={{ textAlign: "center" }}>
+                Here here
+            </div>
+        );
+    }
+}
+
+export default App;
