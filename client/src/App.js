b0bb3de7f840a1990bdd2168628c4fe123b9d277
elena-gancheva <eagan4eva@gmail.com>
2018-07-03 22:10:47 +0300

    [client] add functionality all components to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,83 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from "./components/Game";
+import SelectLevel from "./components/SelectLevel";
+import Loading from "./components/Loading";
+import {Levels} from "./common/constants";
+import socketClientBuilder from './api/socket';
+import Modal from "./components/Modal";
 
 class App extends Component {
 
     constructor(props) {
+        super(props);
+
+        this.state = {
+            modalMessage: "",
+            openModal: false,
+            searchingGame: false,
+            gameStarted: false,
+            yourTurn: false,
+            acronym: "",
+            levelId: "small",
+            level: Levels["small"],
+            client: socketClientBuilder(),
+        };
+
+        this.state.client.registerGameStartHandler((params) => this.initGame(params));
+        this.state.client.registerGameEndHandler((params) => this.onGameEnd(params));
     }
+
+    handleAcronymChange = (acronym) => {
+        this.setState({acronym})
+    };
+
+    handleLevelChange = (levelId) => {
+        this.setState({levelId, level: Levels[levelId]});
+    };
+
+    findGame = () => {
+        const { acronym, level } = this.state;
+        this.setState({ searchingGame: true });
+        this.state.client.findGame(acronym, level, (err) => {
+            if (err)
+                return console.error('Error on findGame', err);
+        })
+    };
+
+    initGame = ({gameId, level, score, playerIdOnTurn, gameStarted}) => {
+        const yourTurn = this.state.client.id() === playerIdOnTurn;
+        this.setState({gameId, level, score, yourTurn, gameStarted, searchingGame: false});
+    };
+
+    onGameEnd = ({message}) => {
+        this.setState({modalMessage: message, openModal: true, gameStarted: false});
+    };
+
+    closeModal = () => this.setState({openModal: false, modalMessage: ""});
 
     render() {
         return (
             <div style={{ textAlign: "center" }}>
-                Here here
+                {(!this.state.searchingGame && !this.state.gameStarted)
+                    && (<SelectLevel
+                            acronym={this.state.acronym}
+                            levelId={this.state.levelId}
+                            handleAcronymChange={this.handleAcronymChange}
+                            handleLevelChange={this.handleLevelChange}
+                            findGame={this.findGame}
+                        />)}
+                {(this.state.searchingGame && !this.state.gameStarted) && (<Loading/>)}
+                {(!this.state.searchingGame && this.state.gameStarted)
+                    && (<Game
+                            gameId={this.state.gameId}
+                            client={this.state.client}
+                            score={this.state.score}
+                            yourTurn={this.state.yourTurn}
+                            rows={this.state.level.rows}
+                            cols={this.state.level.cols}
+                        />)}
+                <Modal show={this.state.openModal} message={this.state.modalMessage} onClose={this.closeModal}/>
             </div>
         );
     }
